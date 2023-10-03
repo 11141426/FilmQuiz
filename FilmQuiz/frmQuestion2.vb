d9d12f508af8dea72f629639f298c345adc4fca1
11141426@college.wlc.ac.uk <11141426@college.wlc.ac.uk>
2023-10-03 12:12:44 +0100

    added code 2 question

--- FilmQuiz/frmQuestion2.vb
+++ FilmQuiz/frmQuestion2.vb
@@ -2,4 +2,15 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblQuestion2.Click
 
     End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If btnAnswer3.Checked Then
+            playerScore = playerScore + 1
+        End If
+
+        frmHighScore.Show()
+        frmHighScore.doScore()
+
+        Me.Hide()
+    End Sub
 End Class
